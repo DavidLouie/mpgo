7bae49706b97c3fdc8626d7eb9400fd43a1bfa9d
David Louie <davidlouie45@gmail.com>
2020-01-25 11:34:51 -0800

    add base UI file

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/davidlouie/mpgo
 
 go 1.13
 
-require github.com/faiface/beep v1.0.2
+require (
+	github.com/faiface/beep v1.0.2
+	github.com/rivo/tview v0.0.0-20200108161608-1316ea7a4b35
+)
--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import (
+/*import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+
     "github.com/davidlouie/mpgo/server"
-)
+)*/
 
 func main() {
-    server.Play("hurt_feelings.mp3")
+    /*for {
+        fmt.Print("Type the name of the song to play: ")
+        scanner := bufio.NewScanner(os.Stdin)
+        for scanner.Scan() {
+            server.Play(scanner.Text())
+        }
+        if scanner.Err() != nil {
+            log.Fatal(scanner.Err())
+        }
+    }*/
+    Init()
 }

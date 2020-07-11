f4de05e4ffdd70c7e06424642c390bd0a4471ac1
David Louie <davidlouie45@gmail.com>
2020-07-11 10:21:28 -0700

    cleanup GetGenres()

--- database/query.go
+++ database/query.go
@@ -18,55 +18,40 @@ type counts struct {
 
 // Returns a map of {genre: {songCount, albumCount}}
 func GetGenres() map[string]counts {
-    genreMap := make(map[string]counts)
-    rows, err := db.Query(`
-        SELECT genre, COUNT(albumId) FROM Albums
+    rows, err = db.Query(`
+        SELECT genre, COUNT(DISTINCT albumId), COUNT(songId)
+        FROM Albums, Songs
+        WHERE Albums.albumId = Songs.albumId
         GROUP BY genre
     `)
     if err != nil {
         log.Fatal(err)
     }
     defer rows.Close()
+    return getGenreMapFromRows(rows)
+}
+
+func getGenreMapFromRows(rows *Rows) map[string]counts {
+    genreMap := make(map[string]counts)
     var (
         genre string
-        count int
+        albumCount int
+        songCount int
     )
     for rows.Next() {
         err := rows.Scan(
             &genre,
-            &count)
+            &albumCount,
+            &songCount
+        )
         if err != nil {
             log.Fatal(err)
         }
-        genreMap[genre] = counts{AlbumCount: count, SongCount: 0}
-    }
-    err = rows.Err()
-    if err != nil {
-        log.Fatal(err)
+        genreMap[genre] = counts{AlbumCount: albumCount, SongCount: songCount}
     }
 
-    rows, err = db.Query(`
-        SELECT genre, COUNT(songId)
-        FROM Albums JOIN Songs ON Albums.albumId = Songs.albumId
-        GROUP BY genre
-    `)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer rows.Close()
-    for rows.Next() {
-        err := rows.Scan(
-            &genre,
-            &count)
-        if err != nil {
-            log.Fatal(err)
-        }
-        currCounts := genreMap[genre]
-        genreMap[genre] = counts{AlbumCount: currCounts.AlbumCount, SongCount: count}
-    }
     err = rows.Err()
     if err != nil {
         log.Fatal(err)
     }
     return genreMap
-}
